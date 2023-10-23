d2da8da527c01a472231c173e991118c8c92d914
Ankit Varshney <lyfflame45@gmail.com>
2023-10-23 12:07:12 +0530

    fix bug

--- routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const authRouter = express.Router();
-const { userSignUp, userLogin } = require('../controllers/authController');
-const authenticateUser = require('../middleware/authMiddleware');
-
-authRouter.post('/signup', signupRouter);
-authRouter.post('/login',loginRouter);
-
-module.exports = authRouter;
--- routes/userRouters/resetPassRoute.js
+++
@@ -1,9 +0,0 @@
-const express =require("express");
-const { resetPassword } = require("../../controllers/userController");
-
-const resetPassRoute= express.Router();
-
-resetPassRoute.post('/', async function(req, res) {
-    await resetPassword(req, res);
-})
-module.exports = resetPassRoute;
