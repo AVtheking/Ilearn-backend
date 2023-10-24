856f4bdb7754c792433a12a207b0533962eb662b
Manya Aggarwal <146344077+Manya1101@users.noreply.github.com>
2023-10-25 00:38:57 +0530

    Added Resend Password

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -7,6 +7,7 @@ authRouter.post("/verify-email", authCtrl.verifyEmail);
 authRouter.post("/sign-in", authCtrl.signIn);
 authRouter.post("/forget-password", authCtrl.forgetPassword);
 authRouter.post("/verify-otp", authCtrl.verifyOtp);
+authRouter.post("/resend-otp", authCtrl.resendOtp);
 authRouter.post("/change-password", authCtrl.changePassword);
 
 module.exports = authRouter;
