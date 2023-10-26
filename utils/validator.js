d33f09a9abfd1f6c3e9c3cdbac623b4295eb3aac
Ankit Varshney <lyfflame45@gmail.com>
2023-10-26 07:43:02 +0530

    bug fix

--- utils/validator.js
+++ utils/validator.js
@@ -2,8 +2,7 @@ const Joi = require("joi");
 
 const authSchema = Joi.object({
   username: Joi.string().required(),
-  first_name: Joi.string().required(),
-  last_name: Joi.string().required(),
+
   email: Joi.string().email().required(),
   password: Joi.string().required(),
 });
