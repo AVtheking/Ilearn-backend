945ec475691be95c321bf291f10707c73ce3ad1c
Manya Aggarwal <146344077+Manya1101@users.noreply.github.com>
2023-10-28 00:42:19 +0530

    Modified Email verification

--- utils/mailer.js
+++ utils/mailer.js
@@ -14,7 +14,22 @@ const sendmail = async (email, otp) => {
     from: process.env.EMAIL,
     to: email,
     subject: "Email verification code",
-    text: `Your OTP is ${otp}`,
+    // text: `Your OTP is ${otp}`,
+    html: 
+    ` <p style="font-size: 16px;"> Hi there ,</p>
+      <p style="font-size: 16px;>Thank you for using our service. To verify your identity, we have sent you a 
+       one-time password (OTP).Please find your OTP below:
+      </p>
+      <p style="font-size: 25px; letter-spacing: 2px; color: lightgreen;" ><strong> OTP : ${otp}
+      </strong><p>
+      <p style="font-size: 16px;>Please do not share this OTP with anyone, as it is used to verify your identity.
+      </p>
+      <p style="font-size: 16px;>If you did not request this OTP, please ignore this email.Thank you for 
+       choosing our service.
+      </p>
+      <p style="font-size: 16px;>Best regards,</p>
+      <p style="font-size: 16px;>Team iLearn</p>  `  , 
+
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
