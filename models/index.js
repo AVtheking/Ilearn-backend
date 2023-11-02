87b9c2214928b3fcae9c782d351a438b8ed3041c
Ankit Varshney <lyfflame45@gmail.com>
2023-11-02 12:40:05 +0530

    added upload video middleware

--- models/index.js
+++ models/index.js
@@ -3,3 +3,4 @@ module.exports.Otp = require("./otp_model");
 module.exports.Course = require("./course");
 module.exports.Video = require("./video");
 module.exports.Category = require("./category");
+
