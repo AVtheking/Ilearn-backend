8ee391ab14c89f089a48704dec850cbf91126c88
Ankit Varshney <lyfflame45@gmail.com>
2023-11-04 00:53:08 +0530

    added publish-course api

--- models/message.js
+++ models/message.js
@@ -1,13 +1,22 @@
 const mongoose = require("mongoose");
-const messageSchema = new mongoose.Schema({
-  text: {
-    type: String,
-    required: true,
+const messageSchema = new mongoose.Schema(
+  {
+    courseId: {
+      type: String,
+    },
+
+    text: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 const Message = new mongoose.model("Message", messageSchema);
-module.exports=Message
+module.exports = Message;
