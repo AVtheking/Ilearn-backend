ab7d45cea809c4dbe441708981a864048d693c32
Ankit Varshney <lyfflame45@gmail.com>
2023-11-03 20:03:08 +0530

    added real time chat

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ const { authRouter, teacherRouter, courseRouter } = require("./routes");
 require("dotenv").config();
 
 const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const { Message } = require("./models");
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
 app.use(
   cors({
@@ -31,9 +40,24 @@ app.use(courseRouter, errorMiddleware);
 
 const PORT = process.env.PORT || 5000;
 
+//socket connection here
+io.on("connection", (socket) => {
+  console.log(`A socket connection to the server has been made: ${socket.id}`);
+  socket.on("message", async (message) => {
+    console.log("received a message", message);
+    const { text, userId } = message;
+    let message = new Message({
+      text,
+      user: userId,
+    });
+    message = await message.save();
+    io.emit("message", message);
+  });
+});
+
 mongoose.connect(process.env.DB).then(() => {
   console.log("connection is successful");
-  app.listen(PORT, "0.0.0.0", () => {
+  server.listen(PORT, "0.0.0.0", () => {
     console.log(`Server is running on port ${PORT}`);
   });
 });
