290649805029cccf31b03968b695a6b71b229b71
Ankit Varshney <lyfflame45@gmail.com>
2023-11-04 19:54:05 +0530

    used aggreation pipeline

--- controllers/course_controller.js
+++ controllers/course_controller.js
@@ -22,10 +22,55 @@ const courseCtrl = {
   getCoursesByCategory: async (req, res, next) => {
     try {
       const category = req.params.category;
-      const courses = await Course.find({ category })
-        .sort("-createdAt")
-        .populate("videos", "_id videoTitle videoUrl")
-        .populate("createdBy", "_id username name");
+      // const courses = await Course.find({ category, isPublished: true })
+      //   .sort("-createdAt")
+      //   .populate("videos", "_id videoTitle videoUrl")
+      //   .populate("createdBy", "_id username name");
+
+      const courses = await Course.aggregate([
+        {
+          $match: {
+            category,
+            isPublished: true,
+          },
+        },
+        {
+          $sort: {
+            createdAt: -1,
+          },
+        },
+        {
+          $lookup: {
+            from: "Video",
+            localField: "videos",
+            foreignField: "_id",
+            as: "videos",
+          },
+        },
+        {
+          $lookup: {
+            from: "User",
+            localField: "createdBy",
+            foreignField: "_id",
+            as: "createdBy",
+          },
+        },
+        {
+          $project: {
+            _id: 1,
+            title: 1,
+            description: 1,
+            price: 1,
+            duration: 1,
+            totalStudents: 1,
+            category: 1,
+            rating: 1,
+            thumbnail: 1,
+            videos: { _id: 1, videoTitle: 1, videoUrl: 1 },
+            createdBy: { _id: 1, username: 1, name: 1 },
+          },
+        },
+      ]);
       if (!courses) {
         return next(
           new ErrorHandler(400, "No course is available with selected category")
