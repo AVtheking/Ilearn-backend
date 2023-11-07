fc0ca7d08da1490b0dcd03a0fe075350ca86e630
Ankit Varshney <lyfflame45@gmail.com>
2023-11-08 02:20:22 +0530

    added thumbnail for the course

--- routes/courseRouter.js
+++ routes/courseRouter.js
@@ -14,8 +14,9 @@ courseRouter.post("/add-cart/:courseId", auth, courseCtrl.addCourseToCart);
 courseRouter.post("/add-wishlist", auth, courseCtrl.addToWishlist)
 courseRouter.delete("/delete-cart/:courseId", auth, courseCtrl.deleteCourseFromCart);
 courseRouter.delete("/delete-wishlist",auth,courseCtrl.deleteCourseFromWishlist)
-module.exports = courseRouter;
+
 courseRouter.get("/search-course", courseCtrl.searchCourses);
 courseRouter.get("/getpopularcourse", courseCtrl.getPopularCourses);
 courseRouter.post("/rate-course",auth,courseCtrl.rateCourse );
 courseRouter.post('/enroll/:courseId', auth, courseCtrl.enrollCourse);
+module.exports = courseRouter;
--- routes/teacherRouter.js
+++ routes/teacherRouter.js
@@ -4,7 +4,7 @@ const { teacherCtrl } = require("../controllers");
 const {auth,uploadVideo} = require("../middlewares");
 
 teacherRouter.patch("/become-instructor", auth, teacherCtrl.becomeTeacher);
-teacherRouter.post("/create-course", auth, teacherCtrl.createCourse);
+teacherRouter.post("/create-course", auth,uploadImage, teacherCtrl.createCourse);
 teacherRouter.post("/upload-video/:courseId", auth, uploadVideo, teacherCtrl.uploadVideo_toCourse);
 teacherRouter.post("/publish-course/:courseId", auth, teacherCtrl.publishCourse);
 <<<<<<< HEAD
