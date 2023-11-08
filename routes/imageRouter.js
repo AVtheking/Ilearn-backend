80b77df59f6c5c0d61ba6780202c945e12803c51
Ankit Varshney <lyfflame45@gmail.com>
2023-11-08 14:00:50 +0530

    fix bugs

--- routes/imageRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { auth } = require("../middlewares");
-const imageRouter = express.Router();
-const imageCtrl = require("../controllers/image_controller");
-
-imageRouter.post("/upload", auth, imageCtrl.uploadImage);
-//imageRouter.post ("/upload", auth );
-module.exports = imageRouter;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 module.exports.authRouter = require("./authRouter");
 module.exports.teacherRouter = require("./teacherRouter");
 module.exports.courseRouter = require("./courseRouter");
-module.exports.imageRouter = require("./imageRouter");
+// module.exports.imageRouter = require("./imageRouter");
 module.exports.commentRouter = require("./commentRouter");
