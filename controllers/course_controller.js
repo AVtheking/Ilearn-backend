8ab7fb7d73052cf12adc10f92aaae9826f9337f5
Ankit Varshney <lyfflame45@gmail.com>
2023-11-08 13:57:15 +0530

    rebased

--- controllers/course_controller.js
+++ controllers/course_controller.js
@@ -203,121 +203,36 @@ const courseCtrl = {
       next(e)
     }
   },
-  searchCourses :async (req, res, next) => {
-    try {
-      const query = req.query.coursetitle; 
-
-      const courses = await Course.find({
-        $or: [
-          { title: { $regex: query, $options: 'i' } }, 
-          { description: { $regex: query, $options: 'i' } }, 
-        ],
-      });
   
-      res.render('course-search', { courses });
-    } catch (error) {
-     //res.status(500).json({ error: 'Error searching for courses.' });
-      next(error); 
-    }
-  },
  
-  enrollCourse : async (req, res, next) => {
-  try {
-    const courseId = req.params.courseId; 
-    const userId = req.user.id; 
-
-    const existingEnrollment = await Enrollment.findOne({ courseId, userId });
-    if (existingEnrollment) {
-    return next(new ErrorHandler(400, "You are already enrolled in this course."));
-    }
-
-    const newEnrollment = new Enrollment({ courseId, userId });
-    await newEnrollment.save();
-
-    res.json({ 
-      message: 'Enrollment successful'
-     });
-     
-  } catch (err) {
-    next(err);
-  }
-},
-  getPopularCourses: async (req, res, next) => {
-    try {
-
-  // const popularCourses = await Course.find({ isPopular: true })
-  //     .sort("-createdAt")
-  //     .populate("videos", "_id videoTitle videoUrl")
-  //     .populate("createdBy", "_id username name");
-      const popularCourses = await Course.aggregate([
-        {
-          $match: {
-            isPopular: true,
-            isPublished: true,
-          },
-        },
-        {
-          $sort: {
-            createdAt: -1,
-          },
-        },
-        {
-          $lookup: {
-            from: "Video",
-            localField: "videos",
-            foreignField: "_id",
-            as: "videos",
-          },
-        },
-        {
-          $lookup: {
-            from: "User",
-            localField: "createdBy",
-            foreignField: "_id",
-            as: "createdBy",
-          },
-        },
-        {
-          $project: {
-            _id: 1,
-            title: 1,
-            description: 1,
-            price: 1,
-            duration: 1,
-            totalStudents: 1,
-            category: 1,
-            rating: 1,
-            thumbnail: 1,
-            videos: { _id: 1, videoTitle: 1, videoUrl: 1 },
-            createdBy: { _id: 1, username: 1, name: 1 },
-          },
-        },
-      ]);
-
-      if (!popularCourses || popularCourses.length === 0) {
-        // return next(
-        //   new ErrorHandler (400, "No popular courses are available.")
-        // );
-        res.status(400).json("do not have any ")
-      }
-
-      
-    } catch (e) {
-      next(e);
-    }
-  },
   searchCourses: async (req, res, next) => {
     try {
       const query = req.query.coursetitle;
 
-      const courses = await Course.find({
-        $or: [
-          { title: { $regex: query, $options: "i" } },
-          { description: { $regex: query, $options: "i" } },
-        ],
-      });
+      // const courses = await Course.find({
+      //   $or: [
+      //     { title: { $regex: query, $options: "i" } },
+      //     { description: { $regex: query, $options: "i" } },
+      //   ],
+      // });
 
-      res.render("course-search", { courses });
+      // res.render("course-search", { courses });
+      // const courses = await Course.find();
+      // const options = {
+      //   keys: ['title'],
+      //   includeScore: true,
+
+      // }
+      // const fuse = new Fuse(courses, options);
+      // const searchquery = req.query.coursetitle
+      // const results = fuse.search(searchquery);
+      // res.json({
+      //   success: true,
+      //   message: "List of courses",
+      //   data: {
+      //     results
+      //   }
+      // })
     } catch (error) {
       //res.status(500).json({ error: 'Error searching for courses.' });
       next(error);
@@ -543,48 +458,7 @@ const courseCtrl = {
         next(error);
       }
     },
-  
-  rateCourse: async (req, res, next) => {
-    try {
-      const { courseId, rating, weight } = req.body;
 
-      const course = await Course.findById(courseId);
-      
-
-      if (!course) {
-        return res.status(404).json({ message: "Course not found" });
-      }
-
-      course.ratings.push({
-        rating,
-        weight,
-      });
-
-      await course.save();
-
-      let totalWeightedRating = 0;
-      let totalWeight = 0;
-
-      for (const r of course.ratings) {
-        totalWeightedRating += r.rating * r.weight;
-        totalWeight += r.weight;
-      }
-
-      const weightedAverageRating = totalWeightedRating / totalWeight;
-
-      course.popularity = calculatePopularity(
-        weightedAverageRating,
-        course.ratings.length
-      );
-
-      course.averageRating = weightedAverageRating;
-      await course.save();
-
-      res.json({ message: "Rating submitted successfully" });
-    } catch (error) {
-      next(error);
-    }
-  },
 };
 
 module.exports = courseCtrl;
