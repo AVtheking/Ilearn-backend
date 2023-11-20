380b620a9d4d7755303592d80d671259d7c71937
Ankit Varshney <lyfflame45@gmail.com>
2023-11-20 20:00:08 +0530

    bugs

--- routes/courseRouter.js
+++ routes/courseRouter.js
@@ -4,7 +4,11 @@ const { courseCtrl, videoCtrl } = require("../controllers");
 const courseRouter = express.Router();
 
 courseRouter.get("/getCourse", auth, courseCtrl.getCourses);
-courseRouter.get("/getCourseBycategory/:category",auth,courseCtrl.getCoursesByCategory);
+courseRouter.get(
+  "/getCourseBycategory/:category",
+  auth,
+  courseCtrl.getCoursesByCategory
+);
 courseRouter.get("/getCourseById/:courseId", auth, courseCtrl.getCourseByid);
 courseRouter.get("/video/:courseId/lecture/:lectureId", videoCtrl.streamVideo);
 courseRouter.get("/getCategoriesName", auth, courseCtrl.getCategoriesName);
@@ -12,19 +16,19 @@ courseRouter.get("/getCategoriesData", auth, courseCtrl.getCategoriesData);
 courseRouter.get("/get-reviews/:courseId", auth, courseCtrl.getReviews);
 courseRouter.get("/get-popular-course", auth, courseCtrl.getPopularCourses);
 
-
 courseRouter.get("/search-course", courseCtrl.searchCourses);
 courseRouter.get("/get-popular-course", courseCtrl.getPopularCourses);
 courseRouter.get("/download-notes/:courseId", auth, courseCtrl.downloadNotes);
-courseRouter.get("/download-video/:courseId/lecture/:lectureId", auth, courseCtrl.downloadVideo);
-
+courseRouter.get(
+  "/download-video/:courseId/lecture/:lectureId",
+  auth,
+  courseCtrl.downloadVideo
+);
 
 courseRouter.post("/rate-course", auth, courseCtrl.rateCourse);
-
 
 courseRouter.patch("/edit-Review/", auth, courseCtrl.editReview);
 
 courseRouter.delete("/delete-Review/", auth, courseCtrl.deleteReview);
-
 
 module.exports = courseRouter;
